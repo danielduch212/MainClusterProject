6076066cccc1419c0e3bf6934f7d628b3eca90b6
Daniel Duchnowicz <72360092+danielduch212@users.noreply.github.com>
2024-12-04 16:17:15 +0100

    added tf file

---
+++ node-pod-adding/node-pod-adding.tf
@@ -0,0 +1,40 @@
+resource "aws_iam_role" "node_role" {
+  name = "eks-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action    = "sts:AssumeRole",
+      Effect    = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
+
+resource "aws_role_policy_attachment" "node_policy" {
+	role	= aws_iam_role.node_role.name
+	policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "cni_policy" {
+	role       = aws_iam_role.node_role.name
+  	policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_eks_node_group" "node_group" {
+  cluster_name    = aws_eks_cluster.eks_cluster.name
+  node_role_arn   = aws_iam_role.node_role.arn
+  subnet_ids      = module.vpc.private_subnets
+  node_group_name = "my-node-group"
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  instance_types = ["t3.medium"]
+}
