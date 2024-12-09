1f1355d14c8369172ba78e1539118c6e62dc6a06
Daniel Duchnowicz <72360092+danielduch212@users.noreply.github.com>
2024-12-09 18:20:36 +0100

    Added NAT gateway AWS.tf

--- eks-cluster/eks_cluster.tf
+++ eks-cluster/eks_cluster.tf
@@ -21,6 +21,10 @@ module "vpc" {
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   private_subnets = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 
+  create_nat_gateway    = true
+  single_nat_gateway    = true
+  enable_dns_hostnames  = true
+  enable_dns_support    = true
 
   public_subnet_tags = {
     "kubernetes.io/cluster/my-eks-cluster" = "shared"
