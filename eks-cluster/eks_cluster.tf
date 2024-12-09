e8502c80c63aea7cc45ac9ac69b918dfb773f2ff
Daniel Duchnowicz <72360092+danielduch212@users.noreply.github.com>
2024-12-09 18:26:45 +0100

    Update eks_cluster.tf

--- eks-cluster/eks_cluster.tf
+++ eks-cluster/eks_cluster.tf
@@ -21,7 +21,7 @@ module "vpc" {
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   private_subnets = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 
-  create_nat_gateways   = true
+  enable_nat_gateway   = true
   single_nat_gateway    = true
   enable_dns_hostnames  = true
   enable_dns_support    = true
